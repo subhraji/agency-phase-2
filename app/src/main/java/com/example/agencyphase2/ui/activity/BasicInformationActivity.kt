d46efeb13ca6a4e59f467b7bce9d565828439faf
subhrajit <subhrajit.deb@ekodus.com>
2022-11-14 17:57:48 +0530

    registration form fix - 14/11/2022

--- app/src/main/java/com/example/agencyphase2/ui/activity/BasicInformationActivity.kt
+++ app/src/main/java/com/example/agencyphase2/ui/activity/BasicInformationActivity.kt
@@ -13,6 +13,7 @@ class BasicInformationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBasicInformationBinding
     val legalStructureList: Array<String> =  arrayOf("Select legal structure", "Solo Proprietorship", "Partnership", "Corporation", "Limited Liability Company")
     val orgTypeList: Array<String> =  arrayOf("Select organisation type", "For profit", "Non profit")
+    val stateList: Array<String> =  arrayOf("State","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","DC")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +31,8 @@ class BasicInformationActivity : AppCompatActivity() {
 
         //spinner
         setUpLsSpinner()
-        setUpOrgType()
+        setUpOrgTypeSpinner()
+        setUpStateSpinner()
     }
 
     private fun setUpLsSpinner(){
@@ -53,10 +55,30 @@ class BasicInformationActivity : AppCompatActivity() {
         }
     }
 
-    private fun setUpOrgType(){
+    private fun setUpOrgTypeSpinner(){
         val arrayAdapter = ArrayAdapter(this, R.layout.simple_spinner_dropdown_item,orgTypeList)
         binding.orgTypeSpinner.adapter = arrayAdapter
         binding.orgTypeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener,
+            AdapterView.OnItemClickListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+        }
+    }
+
+    private fun setUpStateSpinner(){
+        val arrayAdapter = ArrayAdapter(this, R.layout.simple_spinner_dropdown_item,stateList)
+        binding.stateSpinner.adapter = arrayAdapter
+        binding.stateSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener,
             AdapterView.OnItemClickListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 
