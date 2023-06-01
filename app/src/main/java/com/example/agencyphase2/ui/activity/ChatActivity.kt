49e7d03f6c900ede2be5f399dfb82ab335d552ad
subhrajit <subhrajit.deb@ekodus.com>
2023-06-01 17:39:24 +0530

    bug ix - 1/6/23

---
+++ app/src/main/java/com/example/agencyphase2/ui/activity/ChatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.agencyphase2.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.agencyphase2.R
+import com.example.agencyphase2.databinding.ActivityCareGiverProfileBinding
+import com.example.agencyphase2.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
