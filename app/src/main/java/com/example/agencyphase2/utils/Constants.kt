96bc268a6cff4675941cb33dc7c29cdbce7a8198
subhrajit <subhrajit.deb@ekodus.com>
2023-06-08 17:52:38 +0530

    chat ui- 8/6/23

--- app/src/main/java/com/example/agencyphase2/utils/Constants.kt
+++ app/src/main/java/com/example/agencyphase2/utils/Constants.kt
@@ -3,14 +3,14 @@ package com.example.agencyphase2.utils
 object Constants {
 
     //Dev
-    const val BASE_URL = "http://143.110.244.161/api/agency/"
-    const val PUBLIC_URL = "http://143.110.244.161/"
+    /*const val BASE_URL = "http://143.110.244.161/api/agency/"
+    const val PUBLIC_URL = "http://143.110.244.161/"*/
 
     //Payment
     var PAYMENT_URL = "https://api.stripe.com/v1/"
 
     //Live
-    /*const val BASE_URL = "http://134.122.119.7/api/agency/"
-    const val PUBLIC_URL = "http://134.122.119.7/"*/
+    const val BASE_URL = "http://134.122.119.7/api/agency/"
+    const val PUBLIC_URL = "http://134.122.119.7/"
 
 }
