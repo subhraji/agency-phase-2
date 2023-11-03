2b8d60638a7c8017ee29c85f978606fc7798e84e
subhrajit <subhrajit.deb@ekodus.com>
2023-11-03 15:23:30 +0530

    bid now - 3/11/23

--- app/src/main/java/com/example/agencyphase2/utils/Constants.kt
+++ app/src/main/java/com/example/agencyphase2/utils/Constants.kt
@@ -2,9 +2,13 @@ package com.example.agencyphase2.utils
 
 object Constants {
 
+
+    const val BASE_URL = "http://peaceworc.com/api/agency/"
+    const val PUBLIC_URL = "http://peaceworc.com/"
+
     //Dev
-    const val BASE_URL = "http://143.110.244.161/api/agency/"
-    const val PUBLIC_URL = "http://143.110.244.161/"
+    /*const val BASE_URL = "http://143.110.244.161/api/agency/"
+    const val PUBLIC_URL = "http://143.110.244.161/"*/
 
     //Payment
     var PAYMENT_URL = "https://api.stripe.com/v1/"
